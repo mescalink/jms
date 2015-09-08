9f71f17f33039c7109e3cdd9a0b40f28f66e8b7d
Andrey Kolesnikov <mescalink@gmail.com>
2015-09-08 13:32:04 +0200

    -Добавлена статическая страница -добавлен блок отзывов слева (карусель)

--- js/common.js
+++ js/common.js
@@ -1,8 +1,31 @@
 $(document).ready(function() {
   $('#quotes ul li').quovolver(800,15000);
+
+var review = $("#review-slider-left");
+ review.owlCarousel({
+		items :1,
+		autoPlay: true,
+		autoHeight:true
+	});
+
+	$(".review-slider-container .next").click(function(){
+		//alert("rfgdfgdfg");
+		review.trigger("owl.next");
+	});
+	$(".review-slider-container .prev").click(function(){
+		//alert ("ergtdergt");
+		review.trigger("owl.prev");
+	});
+
+
 	//Таймер обратного отсчета
 	//Документация: http://keith-wood.name/countdown.html
 	//<div class="countdown" date-time="2015-01-07"></div>
+
+
+
+
+
 	var austDay = new Date($(".countdown").attr("date-time"));
 	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
 
